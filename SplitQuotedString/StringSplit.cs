3f8189cdb17d6a79b3b2291c8a97be3c1107dc05
Reister Hansjoerg <ret2fe@bosch.com>
2025-09-17 14:50:40 +0200

    Add TestProject1 and refactor StringSplit functionality
    - Updated the solution file to include a new project "TestProject1".
    - Refactored the `StringSplit` class to streamline quote and delimiter handling, and added default character arrays.
    - Enabled parallel test execution in `MSTestSettings.cs`.
    - Created a new test class `Test1` with a method to validate `GetTokens` behavior for empty strings.
    - Established `TestProject1.csproj` with .NET 8.0 SDK and necessary package references.

--- SplitQuotedString/StringSplit.cs
+++ SplitQuotedString/StringSplit.cs
@@ -4,106 +4,93 @@ using System.Text;
 
 namespace hr.Tools;
 
-    /// <summary>
-    /// yet another string splitter
-    /// </summary>
-    public static class StringSplit
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="source">the string to split</param>
-        /// <param name="includeEmpty"></param>
-        /// <param name="quotechars">a set of quote characters, if null the default set is single quote (') and double quote (")</param>
-        /// <param name="delimiters">a set of delimiter (separator) characters</param>. if null the default is space ( ) and tabulator (\t)
-        /// <returns></returns>
-        public static IEnumerable<string> GetTokens(
-            string source,
-            bool includeEmpty = false,
-            char[] quotechars = null,
-            char[] delimiters = null)
-        {
-            if (source is null)
-            {
-                throw new System.ArgumentNullException(nameof(source));
-            }
+/// <summary>
+/// yet another string splitter
+/// </summary>
+public static class StringSplit
+{
+	public static readonly char[] DefaultQuoteChars = ['"', '\''];
+	public static readonly char[] DefaultDelimiters = [' ', '\t'];
+	/// <summary>
+	/// splits a string into tokens, honoring quoted substrings
+	/// </summary>
+	/// <param name="source">the string to split</param>
+	/// <param name="includeEmpty"></param>
+	/// <param name="quotechars">a set of quote characters, if null the default set is single quote (') and double quote (")</param>
+	/// <param name="delimiters">a set of delimiter (separator) characters</param>. if null the default is space ( ) and tabulator (\t)
+	/// <returns></returns>
+	public static IEnumerable<string> GetTokens(
+			string source,
+			bool includeEmpty = false,
+			char[]? quotechars = null,
+			char[]? delimiters = null)
+	{
+		quotechars ??= DefaultQuoteChars;
+		delimiters ??= DefaultDelimiters;
 
-            if (quotechars == null)
-            {
-                quotechars = new char[] { '"', '\'' };
-            }
+		var currentToken = new StringBuilder();
+		var inQuote = false;
+		var currentQuote = (char)0;
+		bool shouldReturn() => includeEmpty || currentToken.Length > 0;
+		
+		foreach (var c in source)
+		{
+			// did we find a quote
+			if (quotechars.Contains(c))
+			{
+				if (inQuote)
+				{
+					if (currentQuote == c)
+					{
+						// close quote by returning token after empty check
+						if (shouldReturn())
+						{
+							yield return currentToken.ToString(); // scannedTokens.Add(currentToken.ToString());
+						}
+						currentToken.Clear();
+						// toggle inquote
+						inQuote = !inQuote;
+					}
+					else
+					{
+						currentToken.Append(c);
+					}
+				}
+				else
+				{
+					currentQuote = c;
+					inQuote = true;
+				}
+			}
 
-            if (delimiters == null)
-            {
-                delimiters = new char[] { ' ', '\t' };
-            }
-
-            var currentToken = new StringBuilder();
-            var inQuote = false;
-            var currentQuote = (char)0;
-            bool shouldReturn()
-            {
-                return includeEmpty || currentToken.Length > 0;
-            }
-            // List<string> scannedTokens = new List<string>();
-            foreach (var c in source ?? string.Empty)
-            {
-                // did we find a quote
-                if (quotechars.Contains(c))
-                {
-                    if (inQuote)
-                    {
-                        if (currentQuote == c)
-                        {
-                            // close quote by returning token after empty check
-                            if (shouldReturn())
-                            {
-                                yield return currentToken.ToString(); // scannedTokens.Add(currentToken.ToString());
-                            }
-                            currentToken.Clear();
-                            // toggle inquote
-                            inQuote = !inQuote;
-                        }
-                        else
-                        {
-                            currentToken.Append(c);
-                        }
-                    }
-                    else
-                    {
-                        currentQuote = c;
-                        inQuote = true;
-                    }
-                }
-
-                // did we find a delimiter
-                else if (delimiters.Contains(c))
-                {
-                    // are we NOT inside a quote
-                    if (!inQuote)
-                    {
-                        // return token after empty check
-                        if (shouldReturn())
-                        {
-                            yield return currentToken.ToString();
-                        }
-                        currentToken.Clear();
-                    }
-                    else
-                    {
-                        // add to current token
-                        currentToken.Append(c);
-                    }
-                }
-                else
-                {
-                    currentToken.Append(c);
-                }
-            }
-            // handle end of loop
-            if (shouldReturn())
-            {
-                yield return currentToken.ToString();
-            }
-        }
-    }
+			// did we find a delimiter
+			else if (delimiters.Contains(c))
+			{
+				// are we NOT inside a quote
+				if (!inQuote)
+				{
+					// return token after empty check
+					if (shouldReturn())
+					{
+						yield return currentToken.ToString();
+					}
+					currentToken.Clear();
+				}
+				else
+				{
+					// add to current token
+					currentToken.Append(c);
+				}
+			}
+			else
+			{
+				currentToken.Append(c);
+			}
+		}
+		// handle end of loop
+		if (shouldReturn())
+		{
+			yield return currentToken.ToString();
+		}
+	}
+}
---
+++ TestProject1/Test1.cs
@@ -0,0 +1,13 @@
+﻿using hr.Tools;
+
+namespace TestProject1;
+
+[TestClass]
+public sealed class Test1
+{
+	[TestMethod]
+	public void TestMethod1()
+	{
+		Assert.IsTrue(StringSplit.GetTokens(string.Empty).ToArray().Length==0);
+	}
+}
