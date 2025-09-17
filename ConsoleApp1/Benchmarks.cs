195313a2ab3195acc46f136efece9847cfc8591d
Hansjörg <hreisterp@hotmail.com>
2025-09-17 23:24:08 +0200

    Add benchmarking project using BenchmarkDotNet
    A new project, `ConsoleApp1`, has been added to the solution
    to benchmark the `QuotedStringSplitter.Split` method.
    
    - Updated `SplitQuotedString.sln` to include `ConsoleApp1`.
    - Added `Benchmarks.cs` with a `Benchmarks` class for performance testing.
    - Created `ConsoleApp1.csproj` targeting .NET 8.0 with `BenchmarkDotNet` dependency.
    - Added `Program.cs` to run benchmarks using `BenchmarkRunner`.
    - Referenced the `SplitQuotedString` project in `ConsoleApp1`.

---
+++ ConsoleApp1/Benchmarks.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace ConsoleApp1;
+
+[MemoryDiagnoser]
+public class Benchmarks
+{
+    [Benchmark]
+    public void SplitQuotedString()
+    {
+        var input = "one two 'three four' five \"six seven\" eight,,nine,,ten 'eleven twelve' \"thirteen fourteen\"";
+        _ = HunnyR.Tools.QuotedStringSplitter.Split(input, new HashSet<char> { ' ', ',' }, new HashSet<char> { '\'', '"' }, treatConsecutiveDelimitersAsOne: true, treatTwoQuotesAsLiteral: false).ToList();
+    }
+}
+
+
