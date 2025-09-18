9e620a5c52273d485ba94f84d3a0d7ef2f917cf5
Reister Hansjoerg <ret2fe@bosch.com>
2025-09-18 10:57:09 +0200

    Refactor and enhance QuotedStringSplitter
    Refactored `QuotedStringSplitter.cs` for consistent formatting and improved readability. Added `TestMethodUseSep` to test integration with the `Sep` library for parsing separated values. Reformatted existing test methods in `Test1.cs` for clarity while preserving logic. Updated `TestProject1.csproj` dependencies: `Microsoft.NET.Test.Sdk` to 17.14.1, `MSTest` to 3.10.4, and added `Sep` library (0.11.1). These changes improve maintainability, expand test coverage, and ensure compatibility with updated dependencies.

--- SplitQuotedString/QuotedStringSplitter.cs
+++ SplitQuotedString/QuotedStringSplitter.cs
@@ -4,9 +4,9 @@ namespace HunnyR.Tools;
 
 public static class QuotedStringSplitterExtensions
 {
-    public static IEnumerable<string> SplitQuoted(
-        this string source, HashSet<char> delimiters, HashSet<char>? quoters = null, bool treatConsecutiveDelimitersAsOne = false, bool treatTwoQuotesAsLiteral = false
-    ) => QuotedStringSplitter.Split(source, delimiters, quoters, treatConsecutiveDelimitersAsOne, treatTwoQuotesAsLiteral);
+	 public static IEnumerable<string> SplitQuoted(
+		  this string source, HashSet<char> delimiters, HashSet<char>? quoters = null, bool treatConsecutiveDelimitersAsOne = false, bool treatTwoQuotesAsLiteral = false
+	 ) => QuotedStringSplitter.Split(source, delimiters, quoters, treatConsecutiveDelimitersAsOne, treatTwoQuotesAsLiteral);
 }
 
 /// <summary>
@@ -14,111 +14,112 @@ public static class QuotedStringSplitterExtensions
 /// </summary>
 public class QuotedStringSplitter
 {
-    /// <summary>
-    /// if set to true (default is false) consecutive delimiters are treated as one e.g. "a,,b" with comma as delimiter will return ["a","b"]
-    /// if false "a,,b" will return ["a","","b"] and ",," will return ["","",""]
-    /// </summary>
-    public bool TreatConsecutiveDelimitersAsOne { get; set; } = false;
+	 /// <summary>
+	 /// if set to true (default is false) consecutive delimiters are treated as one e.g. "a,,b" with comma as delimiter will return ["a","b"]
+	 /// if false "a,,b" will return ["a","","b"] and ",," will return ["","",""]
+	 /// </summary>
+	 public bool TreatConsecutiveDelimitersAsOne { get; set; } = false;
 
-    /// <summary>
-    /// if set to true two consecutive quote characters are treated as a literal quote character 
-    /// </summary>
-    public bool TreatTwoQuotesAsLiteral { get; set; } = false;
+	 /// <summary>
+	 /// if set to true two consecutive quote characters are treated as a literal quote character 
+	 /// </summary>
+	 public bool TreatTwoQuotesAsLiteral { get; set; } = false;
 
-    /// <summary>
-    /// quote characters
-    /// default are double quote(") and single quote (')
-    /// delimiter or quote characters inside a quoted string are treated as normal characters
-    /// can be empty
-    /// '"a.b"' will return ["a.b"] not matter what delimiters
-    /// ''a.b'' will return ["a","b"] if . is the delimiter and TreatTwoQuotesAsLiteral as literal is false else it will return ["'a.b'"]
-    /// </summary>
-    public HashSet<char> Quoters { get; set; } = ['"', '\''];
-    /// <summary>
-    /// delimiters
-    /// default is space and tab
-    /// should contain at least one character. if empty will return the full string as single token
-    /// </summary>
-    public HashSet<char> Delimiters { get; set; } = [' ', '\t'];
+	 /// <summary>
+	 /// quote characters
+	 /// default are double quote(") and single quote (')
+	 /// delimiter or quote characters inside a quoted string are treated as normal characters
+	 /// can be empty
+	 /// '"a.b"' will return ["a.b"] not matter what delimiters
+	 /// ''a.b'' will return ["a","b"] if . is the delimiter and TreatTwoQuotesAsLiteral as literal is false else it will return ["'a.b'"]
+	 /// </summary>
+	 public HashSet<char> Quoters { get; set; } = ['"', '\''];
+	 /// <summary>
+	 /// delimiters
+	 /// default is space and tab
+	 /// should contain at least one character. if empty will return the full string as single token
+	 /// </summary>
+	 public HashSet<char> Delimiters { get; set; } = [' ', '\t'];
 
-    public static readonly HashSet<char> CsvDelimiters = [',', ';', '\t'];
+	 public static readonly HashSet<char> CsvDelimiters = [',', ';', '\t'];
 
-    public static IEnumerable<string> Split(
-        string source, HashSet<char> delimiters, HashSet<char>? quoters = null, bool treatConsecutiveDelimitersAsOne = false, bool treatTwoQuotesAsLiteral = false
-    )
-    {
-        var x = new QuotedStringSplitter()
-        {
-            TreatConsecutiveDelimitersAsOne = treatConsecutiveDelimitersAsOne,
-            TreatTwoQuotesAsLiteral = treatTwoQuotesAsLiteral,
-            Delimiters = delimiters,
-        };
-        if (quoters != null) x.Quoters = quoters;
-        return x.Split(source);
-    }
+	 public static IEnumerable<string> Split(
+		  string source, HashSet<char> delimiters, HashSet<char>? quoters = null, bool treatConsecutiveDelimitersAsOne = false, bool treatTwoQuotesAsLiteral = false
+	 )
+	 {
+		  var x = new QuotedStringSplitter()
+		  {
+				TreatConsecutiveDelimitersAsOne = treatConsecutiveDelimitersAsOne,
+				TreatTwoQuotesAsLiteral = treatTwoQuotesAsLiteral,
+				Delimiters = delimiters,
+		  };
+		  if (quoters != null) x.Quoters = quoters;
+		  return x.Split(source);
+	 }
 
 
-    public IEnumerable<string> Split(
-        string source
-    )
-    {
-        // if is debatable if an empty string should return a single empty item or no items
-        if (source.Length == 0)
-        {
-            yield break;
-        }
-        var currentToken = new StringBuilder();
-        var inQuote = false;
-        var currentQuote = (char)0;
-        char previousChar;
-        var i = 0;
-        var currentChar = (char)0;
-        for (; i < source.Length; i++)
-        {
-            previousChar = currentChar;
-            currentChar = source[i];
-            if (Quoters.Contains(currentChar))
-            {
-                if (!inQuote)
-                {
-                    currentQuote = currentChar;
-                    inQuote = true;
-                }
-                else
-                {
-                    if (currentQuote == currentChar)
-                    {
-                        if (TreatTwoQuotesAsLiteral && previousChar == currentChar) currentToken.Append(currentChar);
-                        inQuote = false;
-                    }
-                    else
-                    {
-                        currentToken.Append(currentChar);
-                    }
-                }
-                continue;
-            }
-            if (Delimiters.Contains(currentChar))
-            {
-                if (inQuote)
-                {
-                    currentToken.Append(currentChar);
-                }
-                else
-                {
-                    yield return currentToken.ToString();
-                    currentToken.Clear();
-                    if (TreatConsecutiveDelimitersAsOne)
-                    {
-                        var k = i + 1;
-                        while (k < source.Length && Delimiters.Contains(source[k])) ++k;
-                        i = k - 1;
-                    }
-                }
-                continue;
-            }
-            currentToken.Append(currentChar);
-        }
-        yield return currentToken.ToString();
-    }
+	 public IEnumerable<string> Split(
+		  string source
+	 )
+	 {
+		  // if is debatable if an empty string should return a single empty item or no items
+		  if (source.Length == 0)
+		  {
+				yield break;
+		  }
+		  var currentToken = new StringBuilder();
+		  var inQuote = false;
+		  var currentQuote = (char)0;
+		  char previousChar;
+		  var i = 0;
+		  var currentChar = (char)0;
+		  for (; i < source.Length; i++)
+		  {
+				previousChar = currentChar;
+				currentChar = source[i];
+				if (Quoters.Contains(currentChar))
+				{
+					 if (!inQuote)
+					 {
+						  currentQuote = currentChar;
+						  inQuote = true;
+					 }
+					 else
+					 {
+						  if (currentQuote == currentChar)
+						  {
+								if (TreatTwoQuotesAsLiteral && previousChar == currentChar) currentToken.Append(currentChar);
+								inQuote = false;
+						  }
+						  else
+						  {
+								currentToken.Append(currentChar);
+						  }
+					 }
+					 continue;
+				}
+				if (Delimiters.Contains(currentChar))
+				{
+					 if (inQuote)
+					 {
+						  currentToken.Append(currentChar);
+					 }
+					 else
+					 {
+						  yield return currentToken.ToString();
+						  currentToken.Clear();
+						  if (TreatConsecutiveDelimitersAsOne)
+						  {
+								var k = i + 1;
+								while (k < source.Length && Delimiters.Contains(source[k])) ++k;
+								i = k - 1;
+						  }
+					 }
+					 continue;
+				}
+				currentToken.Append(currentChar);
+		  }
+		  yield return currentToken.ToString();
+	 }
+
 }
